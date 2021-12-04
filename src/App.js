b464bcd7e1aa1bc1f880a55a249741b96be816e4
kimbeomjin95 <kimbeomjin95@github.com>
2021-12-04 18:23:13 +0900

    117. useEffect() 훅 사용하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,24 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const storedUserLoggedInInfo = localStorage.getItem('isLoggedIn')
+
+  // 무한루프 방지(useEffect 사용)
+  useEffect(() => {
+    if (storedUserLoggedInInfo === '1') {
+      setIsLoggedIn(true)
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1') // 1: login, 2:logout
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
     setIsLoggedIn(false);
   };
 
